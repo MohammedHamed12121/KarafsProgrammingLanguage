931ba487a9d874090a1a63d02f39fadf8fae0a96
MohammedHamed12121 <mh9182731122@gmail.com>
2025-01-07 12:50:13 +0200

    Init

---
+++ Karafs/Program.cs
@@ -0,0 +1,46 @@
+﻿if (args.Length > 1)
+{
+	Console.WriteLine("Usage: Karafs [script]");
+	Environment.Exit(64);
+}
+else if (args.Length == 1)
+{
+	RunFile(args[0]);
+}
+else
+{
+	RunPrompt();
+}
+
+void RunFile(string path)
+{
+    try
+    {
+        string content = File.ReadAllText(path);
+        Run(content);
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine($"Error reading file: {ex.Message}");
+        Environment.Exit(74); // IO error code
+    }
+}
+
+void RunPrompt()
+{
+    while (true)
+    {
+        Console.Write("> ");
+        string line = Console.ReadLine() ?? "";
+
+        if(line == "exit()")
+	        Environment.Exit(64);
+            
+        Run(line);
+    }
+}
+
+void Run(string content)
+{   
+    Console.WriteLine(content);
+}
